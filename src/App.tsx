538bde81b882cdfeee43266253f54ea2a37e0f7d
James Park <jamesdpark1@gmail.com>
2024-07-26 12:06:56 -0400

    first

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+import Nav from './components/nav'
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className="blogContainer">
+          <Nav></Nav>
+          <div>
+            content
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/nav.tsx
@@ -0,0 +1,14 @@
+import './styles/nav.css'
+
+function Nav() {
+  return (
+    <>
+      <div className="nav">
+        <div className="navdir">dir</div>
+        <div className="navdisplay">display</div>
+      </div>
+    </>
+  )
+}
+
+export default Nav
