353c33bb9dc1eb0816cf998baa22cf3a5f518b3b
James Park <jamesdpark1@gmail.com>
2024-07-27 11:04:16 -0400

    tags

--- src/components/writings.tsx
+++ src/components/writings.tsx
@@ -1,11 +1,27 @@
 import './styles/writings.css'
+import { useState } from 'react';
 
 function Writings() {
+  const [devlogs, setDevlogs] = useState(false);
+  const [essays, setEssays] = useState(false);
+  const [college, setCollege] = useState(false);
+
+  const handleToggle = (type:string) => {
+    if (type == "devlogs") setDevlogs(!devlogs)
+    if (type == "essays") setEssays(!essays)
+    if (type == "college") setCollege(!college)
+  };
   return (
     <div className="writingNavContainer">
-        <button>devlogs</button>
-        <button>essays</button>
-        <button>college</button>
+        <button className={devlogs ? 'toggled' : ''} onClick={() => handleToggle("devlogs")}>
+          devlogs
+        </button>
+        <button className={essays ? 'toggled' : ''} onClick={() => handleToggle("essays")}>
+          essays
+        </button>
+        <button className={college ? 'toggled' : ''} onClick={() => handleToggle("college")}>
+          college
+        </button>
     </div>
   )
 }
